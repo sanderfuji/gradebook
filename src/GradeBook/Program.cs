64b8062c3306fbb0c83e864771ea44d02b4317a1
sanderfuji <s.de.bruijn@fujifilm-imaging.nl>
2020-07-07 17:12:14 +0200

    second

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,13 +6,23 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 12.7, 10.3, 6.11 };
+
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            System.Console.WriteLine(result);
+
             if (args.Length > 0)
             {
-                Console.WriteLine($"Hello {args[0]} !");
+                Console.WriteLine($"Result: {result} !");
             }
             else
             {
-                Console.WriteLine("hello!");
+                Console.WriteLine("Give an argument");
             }
         }
     }
